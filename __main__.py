cbafb10ff649bd86fc6b69349559f80060d642d2
alexrf45 <fonalex45@gmail.com>
2022-09-09 19:24:01 -0400

    spilt stacks into projects

--- __main__.py
+++ __main__.py
@@ -2,9 +2,18 @@
 
 import pulumi
 from pulumi_aws import s3
+import pulumi_awsx as awsx
+
+vpc = awsx.ec2.DefaultVpc("default-vpc")
+
+pulumi.export("vpc_id", vpc.vpc_id)
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket('test-roland')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
+# open template readme and read contents into stack output
+with open('./Pulumi.README.md') as f:
+    pulumi.export('readme', f.read())
