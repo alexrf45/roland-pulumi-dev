a8f6e5af669a7f931edbd5d4c9ea3fa50f9b2218
alexrf45 <fonalex45@gmail.com>
2022-09-09 21:11:29 -0400

    set user names in iam.user. try new stack next

--- dev/__main__.py
+++ dev/__main__.py
@@ -5,22 +5,58 @@ import json
 from pulumi_aws import s3
 import pulumi_aws as aws
 
+def dev_iam():
 
-policy = aws.iam.Policy("dev_policy",
-    name='dev_policy',
-    path="/",
-    description="development policy",
-    policy=json.dumps({
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Action": ["ec2:Describe*"],
-            "Effect": "Allow",
-            "Resource": "*",
-        }],
-    }))
-    
+    policy = aws.iam.Policy("dev_policy",
+        name='dev_policy',
+        path="/",
+        description="development policy",
+        policy=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Action": ["ec2:Describe*"],
+                "Effect": "Allow",
+                "Resource": "*",
+            }],
+        }))
+        
 
-pulumi.export('iam_dev_arn', policy.arn)
+    pulumi.export('iam_dev_arn', policy.arn)
+
+    dev = aws.iam.User('dev',
+        name='dev',
+        tags={
+            "name":"dev",
+            "group":"dev-team"
+        })
+    dev_0 = aws.iam.User('dev_0',
+        name='dev-0',
+        tags={
+            "name":"dev",
+            "group":"dev-team"
+        })
+
+    devs = aws.iam.Group('devs',
+        path='/')
+
+    dev_test = aws.iam.GroupPolicy('dev-test',
+        group = devs.id,
+        policy=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Action": ["ec2:Describe*"],
+                "Effect": "Allow",
+                "Resource": "*",
+            }],
+        }))
+
+    dev_team = aws.iam.GroupMembership('dev-team',
+        group = devs.id,
+        users=[dev.id, dev_0.id])
+
+    pulumi.export('iam_group_name', dev_team.id)
+
+dev_iam()
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('dev')
