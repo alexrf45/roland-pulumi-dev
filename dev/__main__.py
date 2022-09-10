bc4e635229498fb55c958eed85b1b5131decf9ae
alexrf45 <fonalex45@gmail.com>
2022-09-09 20:42:37 -0400

    added iam policy and export output for arn

--- dev/__main__.py
+++ dev/__main__.py
@@ -1,7 +1,26 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
+import json
 from pulumi_aws import s3
+import pulumi_aws as aws
+
+
+policy = aws.iam.Policy("dev_policy",
+    name='dev_policy',
+    path="/",
+    description="development policy",
+    policy=json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Action": ["ec2:Describe*"],
+            "Effect": "Allow",
+            "Resource": "*",
+        }],
+    }))
+    
+
+pulumi.export('iam_dev_arn', policy.arn)
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('dev')
