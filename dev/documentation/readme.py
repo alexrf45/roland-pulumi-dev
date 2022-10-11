e35446c959839e1de75c9f12ea4d87131bc44b9d
alexrf45 <fonalex45@gmail.com>
2022-10-10 20:37:46 -0400

    clean up. plan to deploy individual projects for different pieces

--- dev/documentation/readme.py
+++
@@ -1,7 +0,0 @@
-"""exports a README and makes it avaliable pulumi dashboard"""
-import pulumi
-
-def read_me():
-    '''open template readme and read contents into stack output'''
-    with open('Pulumi.README.md', encoding="UTF-8") as open_file:
-        pulumi.export('readme', open_file.read())
