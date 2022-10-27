a75295b710c771f342f9dbdd7693420e8925e1ee
alexrf45 <fonalex45@gmail.com>
2022-10-26 23:40:04 -0400

    ugh virtual env is no fun. no more aws_native, just aws so it's simpler

--- dev/ec2_dev/__main__.py
+++ dev/ec2_dev/__main__.py
@@ -1,6 +1,6 @@
 """An AWS Python Pulumi program"""
-import pulumi
-import pulumi_aws_native as aws
+# import pulumi
+# import pulumi_aws_native as aws
 
 from ec2_dev import instance_dev
 
---
+++ dev/vpc_dev/ec2_dev.py
@@ -0,0 +1,39 @@
+'''simele module to create an EC2 Instance'''
+import pulumi
+import pulumi_aws as aws
+
+INSTANCE_TYPE = 't2.micro'
+AMI_ID= 'ami-0aa6e9e11c6482f0a'
+
+def instance_dev():
+    '''creates a simple security group for instance'''
+    group = aws.ec2.SecurityGroup('web-secgrp',
+            description='Enable HTTP/HTTPS access',
+            ingress=[aws.ec2.SecurityGroupIngressArgs(
+                from_port=80,
+                to_port=80,
+                protocol="tcp",
+                cidr_blocks=['0.0.0.0/0'],
+            )],
+            egress=[aws.ec2.SecurityGroupEgressArgs(
+                from_port=80,
+                to_port=80,
+                protocol="tcp",
+                cidr_blocks=["0.0.0.0/0"],
+                ipv6_cidr_blocks=["::/0"],
+                )],
+            tags={
+                "Name": "allow_tls",
+            })
+    user_data = """
+    #!/bin/bash
+    echo "Hello, World!" > index.html
+    nohup python -m SimpleHTTPServer 80 &
+    """  
+    server = aws.ec2.Instance('web-server-www',
+                instance_type=INSTANCE_TYPE,
+                vpc_security_group_ids=[group.id],
+                user_data=user_data,
+                ami=AMI_ID)
+    pulumi.export('public_ip', server.public_ip)
+    pulumi.export('public_dns', server.public_dns)
